9a9b3bb3b036b718ee1aff802b46300e79d659ff
AlexChulo <alex123149@gmail.com>
2024-05-21 21:30:35 +0200

    mysql library added

--- MedicalAppointmentSystem/src/main/java/module-info.java
+++ MedicalAppointmentSystem/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.medicalappointmentsystem {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.medicalappointmentsystem to javafx.fxml;
     exports com.example.medicalappointmentsystem;
